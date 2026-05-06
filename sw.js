360d9526bcbea7c6ca68a753f5ed514c7826a942
Bastiaan Sommeling <basmacm4@MacBook-Air-van-Bastiaan.local>
2026-05-06 12:24:52 +0200

    PWA + dagelijkse ochtendbriefjes van B

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'roma-v1';
+const FILES = ['./', './index.html', './photo.jpg', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
